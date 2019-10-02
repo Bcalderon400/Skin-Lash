c07766a2e844c139464f1ea6f1e25b9dd3c0ed3b
Brian <briancalderon200@gmail.com>
2019-10-01 19:20:45 -0700

    site structure looks like is rendering properly

--- public/js/style.js
+++ public/js/style.js
@@ -1,72 +1,24 @@
-(function($) {
-    /*-----------------------------------------------------*/
-    /* Navigation Menu
-   ------------------------------------------------------ */
-    var toggleButton = $('.menu-toggle'),
-        nav = $('.main-navigation');
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
-    // toggle button
-    toggleButton.on('click', function(e) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-        e.preventDefault();
-        toggleButton.toggleClass('is-clicked');
-        nav.slideToggle();
+            // Store hash
+            var hash = this.hash;
 
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
-
-    // nav items
-    nav.find('li a').on("click", function() {
-
-        // update the toggle button 		
-        toggleButton.toggleClass('is-clicked');
-        // fadeout the navigation panel
-        nav.fadeOut();
-
-    });
-
-
-    /*---------------------------------------------------- */
-    /* Highlight the current section in the navigation bar
-    ------------------------------------------------------ */
-    var sections = $("section"),
-        navigation_links = $("#main-nav-wrap li a");
-
-    sections.waypoint({
-
-        handler: function(direction) {
-
-            var active_section;
-
-            active_section = $('section#' + this.element.id);
-
-            if (direction === "up") active_section = active_section.prev();
-
-            var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
-            navigation_links.parent().removeClass("current");
-            active_link.parent().addClass("current");
-
-        },
-
-        offset: '25%'
-    });
-
-
-    /*---------------------------------------------------- */
-    /* Smooth Scrolling
-    ------------------------------------------------------ */
-    $('.smoothscroll').on('click', function(e) {
-
-        e.preventDefault();
-
-        var target = this.hash,
-            $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 800, 'swing', function() {
-            window.location.hash = target;
-        });
-
-    });
-})
+});
